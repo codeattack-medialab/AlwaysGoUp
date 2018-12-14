97b3dec824da2c3e6e7849d541608c294e6b40e5
40detectives <100275684@alumnos.uc3m.es>
2018-12-14 13:53:31 +0100

    Improvements in HUD visualization
    - Fix overlapping of the number with the UnitsLabel after a few animations played
    - Add shadows and outlines to the text in the HUD for better readability

--- ScoreInMeters.gd
+++ ScoreInMeters.gd
@@ -4,11 +4,18 @@ extends Label
 # var a = 2
 # var b = "textvar"
 
-func _ready():
-	# Called when the node is added to the scene for the first time.
-	# Initialization here
-	pass
+"""
+func _process(delta):
+	# This code commented bellow is for real quick 
+	# testing playing just the HUD.tscn scene with F6 
+	
+	if Input.is_action_just_pressed("jump"):
+		print("traza, detecto el salto")
+		self.text = str(int(self.text) + int(rand_range(11,33)))
+		#Color(0.902,0.445,0,1)
+		$"../../AnimationPlayer".play("height_anim")
+"""
 
 func _on_HUD_height_change(new_height):
 	self.text = str(new_height)
-	$"../AnimationPlayer".play("height_anim")
+	$"../../AnimationPlayer".play("height_anim")
