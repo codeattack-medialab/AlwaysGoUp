532bf548724969443e365ed42b426d544d6803b7
40detectives <9024463+40detectives@users.noreply.github.com>
2020-01-22 17:30:47 +0100

    Get target platform info on game startup

--- Global.gd
+++ Global.gd
@@ -1,5 +1,8 @@
 extends Node
 
+var platform_name : String
+var touchscreen : bool
+var virtual_keyboard : bool
 var current_scene
 var scores = Array()
 var current_score = {meters = 0, ms = 0}
@@ -7,6 +10,18 @@ var current_score = {meters = 0, ms = 0}
 func _ready():
 	var root = get_tree().get_root()
 	current_scene = root.get_child(root.get_child_count() - 1)
+	init_platform_info()
+
+
+func init_platform_info():
+	platform_name = OS.get_name()
+	touchscreen = OS.has_touchscreen_ui_hint()
+	virtual_keyboard = OS.has_virtual_keyboard()
+	if OS.is_debug_build():
+		print ("Platform: " + platform_name)
+		print ("Has touchscreen capabities: " + touchscreen as String)
+		print ("Has virtual keyboard: " + virtual_keyboard as String)
+
 
 func go_to_scene(new_scene):
 	call_deferred("_deferred_go_to_scene", new_scene)
